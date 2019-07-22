ffdcab95b34470dcf37e40af859aa12050ff00cb
zhu.xi <zhu.xi@immomo.com>
2019-07-22 18:25:49 +0800

    [Update] 初始化工程

---
+++ Frameworks/MMNNetwork.framework/Headers/MMNNetwork-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MNBaseRequest+Extension.h"
+#import "MNBaseRequest.h"
+#import "MNURLRequestSerialization.h"
+#import "MNURLResponseSerialization.h"
+#import "MNURLSessionManager.h"
+
+FOUNDATION_EXPORT double MMNNetworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char MMNNetworkVersionString[];
+
